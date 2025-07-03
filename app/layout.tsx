40979a30c81151784285d68857cf553d5df8cc39
Mahmoud  Ali <61747217+MahmoudAliEid@users.noreply.github.com>
2025-07-03 18:57:50 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Bluggenn',
+  description: 'Bluggenn is a Saudi tech brand founded by passionate engineers with a love for smart, simple, and reliable designs. We specialize in mobile accessories—such as power banks, chargers, and cables—meticulously crafted with attention to detail, style, and uncompromising quality.',
+  generator: 'Soft Masters',
 }
 
 export default function RootLayout({
